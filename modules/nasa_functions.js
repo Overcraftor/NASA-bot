c8c83ac4d3bf01aca47bd65b2170b2b89962e7a7
Overcraftor <60623689+Overcraftor@users.noreply.github.com>
2021-05-02 19:13:30 +0200

    Update nasa_functions.js

--- modules/nasa_functions.js
+++ modules/nasa_functions.js
@@ -11,15 +11,17 @@ class nasa_functions {
         https.get(url, res =>{
             if(res.statusCode === 200 && res.headers['content-type'] === "image/png"){
                 message.edit("Réponse de l'api, traitement de l'image...").then();
-                let data = new Stream();
+                let data = '';
 
                 res.on('data', chunk => {
-                    data.push(chunk);
+                    data += chunk;
                 });
 
                 res.on('end', () =>{
                     message.edit("Image trouvé.").then();
-                    message.channel.send(new Discord.MessageAttachment(data)).then().catch(err => console.log("Error found: "+ err));
+                    //message.channel.send(new Discord.MessageAttachment(data)).then().catch(err => console.log("Error found: "+ err));
+                    console.log(data.hdurl);
+                    message.channel.send("Image satellite:", {file:data.hdurl});
                 });
             }else{
                 message.edit("Aucune image trouvé, veuillez indiquer des coordonnées valides !").then();
