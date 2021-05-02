770c9f6f54d4fe745cdb3993d7c5b24685dd6a61
Overcraftor <60623689+Overcraftor@users.noreply.github.com>
2021-05-02 17:41:28 +0200

    Add tag on enable

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = client =>{
-    console.log("Bot connected !")
+    console.log("Bot connected ! (" + client.user.tag + ")")
     client.user.setActivity("By Overcraftor#4841 | Fais " + client.config.prefix + "help").then();
 }
