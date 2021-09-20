d2f55044d46d1f6e15b0dbf6fbf08f6c511b20a7
Overcraftor <60623689+Overcraftor@users.noreply.github.com>
2021-09-20 20:14:36 +0200

    T

--- modules/nasa_functions.js
+++ modules/nasa_functions.js
@@ -23,10 +23,10 @@ class nasa_functions {
                 });
             }else{
                 message.edit("Aucune image trouvé, veuillez indiquer des coordonnées valides !").then();
-                console.log("Erreur : " + res.headers);
+                res.on('data', chunk => {
+                    console.log(chunk);
+                });
                 console.log("Erreur : " + res.headers['content-type']);
-                console.log("Erreur : " + res.headers.err);
-                console.log("Erreur : " + res.headers.error);
             }
         }).on('error', err =>{
             console.log("Erreur lors de la récupération d'une image: " + err);
